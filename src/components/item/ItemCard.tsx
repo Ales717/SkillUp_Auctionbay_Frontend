e1584c2d6c2d923a16e1f4b5d5c8cb5912ce2ef1
Ales717 <prosenjak.ales77@gmail.com>
2024-04-17 14:32:27 +0200

    Item details

--- src/components/item/ItemCard.tsx
+++ src/components/item/ItemCard.tsx
@@ -1,27 +1,33 @@
-import { ItemType } from 'models/item'
+import { ItemTypeId } from 'models/itemId'
 import { FC } from 'react'
 import Badge from 'react-bootstrap/Badge'
+import { Link } from 'react-router-dom'
 
 
 interface Props {
-    item: ItemType
+    item: ItemTypeId
 }
 
 const ItemCard: FC<Props> = ({ item }) => {
+    const itemDetailsPath = `/auctions/itemdetails/${item.id}`
+
     return (
-        <div className='card'>
-            <div className="card-head">
-                <div className='d-flex justify-content-between'>
-                    <Badge pill className='tag-red'>Outbid</Badge>
-                    <Badge pill className="tag-red">24h</Badge>
+        <Link to={itemDetailsPath} style={{ textDecoration: 'none' }}>
+            <div className='card'>
+                <div className="card-head">
+                    <div className='d-flex justify-content-between'>
+                        <Badge pill className='tag-red'>Outbid</Badge>
+                        <Badge pill className="tag-red">24h</Badge>
+                    </div>
+                    <div className="card-title m-0">{item.title}</div>
+                    <div className='card-price m-0'>{item.starting_price} €</div>
                 </div>
-                <div className="card-title m-0">{item.title}</div>
-                <div className='card-price m-0'>{item.starting_price} €</div>
+                <div className="card-img">
+                    <img src={`${process.env.REACT_APP_API_URL}/files/${item.image}`} alt={item.title} />
+                </div>
             </div>
-            <div className="card-img">
-                <img src={`${process.env.REACT_APP_API_URL}/files/${item.image}`} alt={item.title} />
-            </div>
-        </div>
+        </Link>
+
     )
 }
 
